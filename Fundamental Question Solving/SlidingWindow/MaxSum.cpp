fd82758c2cbbe245ae3241192974eb5998a2aa19
sonett <sonettjsaha@gmail.com>
2022-05-28 17:32:57 +0530

    Learned about sliding window technique & solved some basic questions

---
+++ Fundamental Question Solving/SlidingWindow/MaxSum.cpp
@@ -0,0 +1,38 @@
+// Learned new things: *std::max_element() & *std::min_element()
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    std::vector<int> vec = {0};
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int k = 3;
+    int sum = 0;
+
+    int i = 0;
+    int j = 0;
+
+    while(j < size) {
+        sum += arr[j];
+
+        if(j - i + 1 < k) {
+            j++;
+        } else if (j - i + 1 == k) {
+            std::cout << sum << " ";
+            vec.push_back(sum);
+
+            sum -= arr[i];
+            i++;
+            j++;
+        }
+    }
+
+    std::cout << "\nMax sum between those sub-arrays is: " << *(std::max_element(vec.begin(), vec.end())) << std::endl;
+    return 0;
+}
---
+++ Fundamental Question Solving/remove_duplicate.cpp
@@ -0,0 +1,45 @@
+// 65.	WAP to Remove duplicate elements in an array
+
+#include <iostream>
+#include <unordered_map>
+#define N 5
+
+
+// count the frequency
+    // for (int i = 0; i < arr_size; i++) {
+    //     numRay[numRay[i] % arr_size]
+    //         = numRay[numRay[i] % arr_size] + arr_size;
+    // }
+    // cout << "The repeating elements are : " << endl;
+    // for (int i = 0; i < arr_size; i++) {
+    //     if (numRay[i] >= arr_size * 2) {
+    //         cout << i << " " << endl;
+    //     }
+    // }
+
+
+int main()
+{
+    int arr[N];
+    std::cout << "Enter "<< N <<" elements in an array: " << std::endl;
+
+    for(int i = 0; i < N; i++) {
+        std::cin >> arr[i];
+    }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = i + 1; j < N; j++) {
+            if(arr[i] == arr[j]) {
+                arr[j] = 0;
+            }
+        }
+    }
+
+    std::cout << "\nThe array values are: " << std::endl;
+    for(int i = 0; i < N; i++) {
+        if(arr[i] > 0)
+            std::cout << arr[i] << " ";
+    }
+    
+    std::cin.get();
+}
