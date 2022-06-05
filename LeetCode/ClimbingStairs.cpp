47300b29ae2c46fb43b1c921ab8185dcbb60bf53
sonett <sonettjsaha@gmail.com>
2022-06-05 20:25:21 +0530

    Another way to solve climbing stairs leetcode problem

--- LeetCode/ClimbingStairs.cpp
+++ LeetCode/ClimbingStairs.cpp
@@ -4,24 +4,41 @@
 
 int climbStairs(int n)
 {
-    long long one = 1;
-    long long two = 1;
+    long long currentStep = 0;
+    long long nextStep = 1;
 
-    long long steps = 0;
+    long long totalSteps = 0;
 
     for (int i = 1; i <= n; i++)
     {
-        steps = one;
-        one = one + two;
-        two = steps;
+        totalSteps = currentStep + nextStep;
+        currentStep = nextStep;
+        nextStep = totalSteps;
     }
 
-    return steps;
+    return totalSteps;
 }
+
+// int climbStairs(int n)
+// {
+//     long long one = 1;
+//     long long two = 1;
+
+//     long long steps = 0;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         steps = one;
+//         one = one + two;
+//         two = steps;
+//     }
+
+//     return steps;
+// }
 
 int main()
 {
-    int n = 20;
+    int n = 2;
 
     std::cout << climbStairs(n);
     return 0;
